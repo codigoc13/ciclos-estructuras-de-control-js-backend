f287c6e749d0859f2df833c915ee0f64066f99aa
seraleman <seraleman1204@gmail.com>
2022-07-20 17:19:21 -0500

    Se agrega switch

---
+++ src/assets/switch.js
@@ -0,0 +1,18 @@
+const dia = 0 //el 0 es el domingo
+
+switch (dia) {
+  case 0:
+    console.log('domingo')
+    break
+  case 1:
+    console.log('lunes')
+    break
+  case 2:
+    console.log('martes')
+    break
+  case 3:
+    console.log('miércoles')
+    break
+  default:
+    console.log('No es domingo, ni lunes, ni martes, ni miércoles')
+}
